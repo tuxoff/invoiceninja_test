74c9fb682ef304882393aed6772a647236f3cc10
Denis Melskiy <dmelskiy@gmail.com>
2025-06-10 21:49:13 +0200

    fix(horizon): try to fix auth

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -276,7 +276,7 @@ class Handler extends ExceptionHandler
                 $login = 'ronin.login';
                 break;
             default:
-                $login = 'default';
+                $login = 'login';
                 break;
         }
 
