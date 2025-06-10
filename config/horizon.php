d5bd3ccb2ec3ce873bee510042840c1b6eedb081
Denis Melskiy <dmelskiy@gmail.com>
2025-06-10 20:38:09 +0200

    feat(horizon): try to limit process count

--- config/horizon.php
+++ config/horizon.php
@@ -198,7 +198,7 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
+                'maxProcesses' => 2,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
